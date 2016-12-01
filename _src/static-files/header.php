85e530cbd3a29b2ee2f96bcf4a4be48f4d0937b3
Anton Andersson <anton@aaff.se>
2016-12-01 18:49:01 +0100

    added GA

--- _src/static-files/header.php
+++ _src/static-files/header.php
@@ -32,6 +32,15 @@ DesignObjekt is a project by Anton Andersson Form & Funktioner and 1910 Design &
 			<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 		<![endif]-->
 
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-88292482-1', 'auto');
+			ga('send', 'pageview');
+		</script>
+
 	</head>
 	<body>
 		<style>
